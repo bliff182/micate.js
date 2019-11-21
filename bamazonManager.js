9207fc22e1557df4f24aaa06c281f39c697052f8
bliff182 <michael.bither@gmail.com>
2019-11-21 10:17:50 -0500

    manager viewProducts function working, table looks sexy

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,22 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table3');
+
+// var table = new Table({
+//   chars: {
+//     'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+//     , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+//     , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+//     , 'right': '║', 'right-mid': '╢', 'middle': '│'
+//   }
+// });
+
+// table.push( // basic table template - remember to delete
+//   ['foo', 'bar', 'baz']
+//   , ['frob', 'bar', 'quuz']
+// );
+
+// console.log(table.toString());
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -12,5 +29,69 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   // attempting ES6
-  console.log(`connected as id ${ connection.threadId }`);
+  console.log(`connected as id ${connection.threadId}`);
+  showMenu();
 });
+
+function showMenu() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View Products for Sale',
+        'View Low Inventory',
+        'Add to Inventory',
+        'Add New Product',
+        'Exit'
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case 'View Products for Sale':
+          viewProducts();
+          break;
+
+        case 'View Low Inventory':
+          lowInventory();
+          break;
+
+        case 'Add to Inventory':
+          addInventory();
+          break;
+
+        case 'Add New Product':
+          addProduct();
+          break;
+
+        case 'Exit':
+          connection.end();
+          break;
+      }
+    });
+}
+
+function viewProducts() {
+  var query = 'SELECT * FROM products';
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    var table = new Table({
+      head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
+      chars: {
+        'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+        , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+        , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+        , 'right': '║', 'right-mid': '╢', 'middle': '│'
+      }
+    });
+
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+    }
+
+    console.log(table.toString());
+
+  });
+}
